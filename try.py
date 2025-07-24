d0201548ea8576a94f614e6b5fee1514d165170f
merveocal <ocamerve752@gmail.com>
2025-07-24 11:55:41 +0300

    yeni bir satır ekledim

--- try.py
+++ try.py
@@ -1,2 +1,3 @@
 print("git diff de faklılıklar görmek için bir şeyler yazıyorum")
+print("yeni bir satır ekledim")
 print("değişiklikleri çekmeden başka bir değişiklik yapmak için bunu yazdım şimdi de commit atacağım")
